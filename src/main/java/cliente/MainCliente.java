eaa995cc9e917d0d59a30359ddbdf5509acde807
Zagadka10 <hectoralbarranmadrigal1506@gmail.com>
2026-05-09 13:49:18 +0200

    conexion arreglada e interfaz empezada

--- src/main/java/cliente/MainCliente.java
+++ src/main/java/cliente/MainCliente.java
@@ -17,29 +17,20 @@ public class MainCliente {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Iniciando terminal del Gobierno...");
         System.out.println("Buscando conexión con el Laboratorio Hawkins...");
 
         try {
-            // 1. Localizamos el objeto distribuido en la red usando nuestra interfaz común
+            // Localizamos el objeto en la red
             InterfaceHawkins servidor = (InterfaceHawkins) Naming.lookup("//localhost/ObjetoHawkins");
-            
-            System.out.println("¡Conexión establecida con éxito!\n");
+            System.out.println("¡Conexión establecida! Abriendo interfaz gráfica...");
 
-            // 2. Le hacemos un par de preguntas al servidor en tiempo real
-            System.out.println("--- DATOS CLASIFICADOS ---");
-            System.out.println("Sangre recolectada por Vecna: " + servidor.getSangreVecna());
-            System.out.println("Demogorgons creados: " + servidor.getCapturasTotales());
-            System.out.println("Niños detectados en el Bosque: " + servidor.getNumeroNinosEnZona("Bosque"));
-            System.out.println("¿Hay Apagón general?: " + (servidor.isApagonActivo() ? "SÍ" : "NO"));
-            
-            System.out.println("\nSi ves estos datos, la arquitectura RMI funciona perfectamente.");
+            // Abrimos la ventana pasándole el acceso al servidor
+            ClienteGUI ventana = new ClienteGUI(servidor);
+            ventana.setVisible(true);
 
         } catch (Exception e) {
             System.out.println("!!! Error de conexión: No se pudo contactar con el Servidor.");
-            System.out.println("Asegúrate de que el Servidor (Main) está ejecutándose primero.");
             e.printStackTrace();
         }
     }
-    
 }
